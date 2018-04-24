dda18f7ba1b2d47385c390a91e8ac4e3b932c511
Ben Newman <ben@apollographql.com>
2018-04-24 17:13:13 -0400

    Stop needlessly creating new NormalizedCache objects.

--- packages/apollo-cache-inmemory/src/inMemoryCache.ts
+++ packages/apollo-cache-inmemory/src/inMemoryCache.ts
@@ -89,8 +89,12 @@ export class InMemoryCache extends ApolloCache<NormalizedCacheObject> {
       return null;
     }
 
+    const store = (query.optimistic && this.optimistic.length)
+      ? defaultNormalizedCacheFactory(this.extract(true))
+      : this.data;
+
     return readQueryFromStore({
-      store: defaultNormalizedCacheFactory(this.extract(query.optimistic)),
+      store,
       query: this.transformDocument(query.query),
       variables: query.variables,
       rootId: query.rootId,
@@ -115,8 +119,12 @@ export class InMemoryCache extends ApolloCache<NormalizedCacheObject> {
   }
 
   public diff<T>(query: Cache.DiffOptions): Cache.DiffResult<T> {
+    const store = (query.optimistic && this.optimistic.length)
+      ? defaultNormalizedCacheFactory(this.extract(true))
+      : this.data;
+
     return diffQueryAgainstStore({
-      store: defaultNormalizedCacheFactory(this.extract(query.optimistic)),
+      store: store,
       query: this.transformDocument(query.query),
       variables: query.variables,
       returnPartialData: query.returnPartialData,
